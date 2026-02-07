8b075a895dc88161aa0c774845a8129e3fa8e6b5
Vyacheslav Melnikov <robber2005@mail.ru>
2026-02-07 21:29:30 +0300

    Create base_page.py

---
+++ pages/base_page.py
@@ -0,0 +1,27 @@
+import allure
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class BasePage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    @allure.step("Ожидаем элемент {locator}")
+    def find_element(self, locator, time=15):
+        return WebDriverWait(self.driver, time).until(EC.presence_of_element_located(locator))
+
+    @allure.step("Кликаем на элемент {locator}")
+    def click_element(self, locator):
+        WebDriverWait(self.driver, 15).until(EC.element_to_be_clickable(locator)).click()
+
+    @allure.step("Прокручиваем к элементу {locator}")
+    def scroll_to_element(self, locator):
+        element = self.find_element(locator)
+        self.driver.execute_script("arguments.scrollIntoView();", element)
+
+    @allure.step("Вводим текст в поле {locator}")
+    def send_keys(self, locator, text):
+        element = self.find_element(locator)
+        element.clear()
+        element.send_keys(text)
+        
