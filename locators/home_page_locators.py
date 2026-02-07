cc626a542069daadc38431eeec085387f2785e57
Vyacheslav Melnikov <robber2005@mail.ru>
2026-02-07 22:03:39 +0300

    Add COOKIE_BUTTON

--- locators/home_page_locators.py
+++ locators/home_page_locators.py
@@ -6,7 +6,8 @@ class HomePageLocators:
     ORDER_BUTTON_BOTTOM = (By.XPATH, ".//div[contains(@class, 'Home_FinishButton')]//button[text()='Заказать']")
     LOGO_YANDEX = (By.XPATH, ".//a[contains(@class, 'Header_LogoYandex')]")
     LOGO_SCOOTER = (By.XPATH, ".//a[contains(@class, 'Header_LogoScooter')]")
-
+    COOKIE_BUTTON = (By.ID, "rcc-confirm-button")
+    
     @staticmethod
     def faq_question(index):
         return (By.ID, f"accordion__heading-{index}")
