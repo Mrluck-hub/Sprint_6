e2b7139a846b2a9b289f37241f3007e425b0865b
Vyacheslav Melnikov <robber2005@mail.ru>
2026-02-07 19:57:06 +0300

    Create home_page_locators.py

---
+++ locators/home_page_locators.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+
+class HomePageLocators:
+    ORDER_BUTTON_UP = (By.XPATH, ".//div[contains(@class, 'Header_Nav')]//button[text()='Заказать']")
+    ORDER_STATUS_BUTTON = (By.XPATH, ".//div[contains(@class, 'Header_Nav')]//button[text()='Статус заказа']")
+    ORDER_BUTTON_BOTTOM = (By.XPATH, ".//div[contains(@class, 'Home_FinishButton')]//button[text()='Заказать']")
+    LOGO_YANDEX = (By.XPATH, ".//a[contains(@class, 'Header_LogoYandex')]")
+    LOGO_SCOOTER = (By.XPATH, ".//a[contains(@class, 'Header_LogoScooter')]")
+
+    @staticmethod
+    def faq_question(index):
+        return (By.ID, f"accordion__heading-{index}")
+    
+    @staticmethod
+    def faq_answer(index):
+        return (By.ID, f"accordion__panel-{index}")
